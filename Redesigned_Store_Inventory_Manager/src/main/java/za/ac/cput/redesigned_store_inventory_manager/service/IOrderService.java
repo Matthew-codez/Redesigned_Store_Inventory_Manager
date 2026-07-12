86d8d30806b223088048f74304563b5eceffe8f3
Matthew-codez <matthewferreira83@gmail.com>
2026-07-12 20:02:05 +0200

    Add Customer and Order service classes

---
+++ Redesigned_Store_Inventory_Manager/src/main/java/za/ac/cput/redesigned_store_inventory_manager/service/IOrderService.java
@@ -0,0 +1,7 @@
+package za.ac.cput.redesigned_store_inventory_manager.service;
+
+import za.ac.cput.redesigned_store_inventory_manager.domain.Order;
+
+public interface IOrderService extends IService {
+    Order save(Order order);
+}
---
+++ Redesigned_Store_Inventory_Manager/src/main/java/za/ac/cput/redesigned_store_inventory_manager/service/OrderService.java
@@ -0,0 +1,7 @@
+package za.ac.cput.redesigned_store_inventory_manager.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+}
