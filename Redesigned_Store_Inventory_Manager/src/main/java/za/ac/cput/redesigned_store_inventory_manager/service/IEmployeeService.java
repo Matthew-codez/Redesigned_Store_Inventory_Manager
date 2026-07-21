462aa142a5b7cff30dad950b41617f4d0ce912d5
Jaydenchoppa <jayden.choppa19@gmail.com>
2026-07-21 19:49:34 +0200

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit efd2b5a02ee770ee60c4854ac74e2f9fdd548053, reversing
    changes made to 36927400697625c9f67db47e9177b86bdfebc931.

--- Redesigned_Store_Inventory_Manager/src/main/java/za/ac/cput/redesigned_store_inventory_manager/service/IEmployeeService.java
+++
@@ -1,6 +0,0 @@
-package za.ac.cput.redesigned_store_inventory_manager.service;
-
-import za.ac.cput.redesigned_store_inventory_manager.domain.Employee;
-
-public interface IEmployeeService extends IService<Employee, Long> {
-}
--- Redesigned_Store_Inventory_Manager/src/main/java/za/ac/cput/redesigned_store_inventory_manager/service/ISupplierService.java
+++
@@ -1,6 +0,0 @@
-package za.ac.cput.redesigned_store_inventory_manager.service;
-
-import za.ac.cput.redesigned_store_inventory_manager.domain.Supplier;
-
-public interface ISupplierService extends IService<Supplier, Long> {
-}
